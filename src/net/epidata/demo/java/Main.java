cc6d681381d7822841377b53ea846fe9dc101ddb
Damian Reboredo <reboredodamian@hotmail.com>
2024-09-12 18:21:33 -0300

    bug

--- src/net/epidata/demo/java/Main.java
+++ src/net/epidata/demo/java/Main.java
@@ -6,6 +6,12 @@ public class Main {
 
 		System.out.print("hola");
 
+		int target = -5;
+		int num = 3;
+
+		target =- num;  // Noncompliant: target = -3. Is that the intended behavior?
+		target =+ num; // Noncompliant: target = 3
+		
 	}
 
 }
